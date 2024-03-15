6576db6617b94cde10851b32fc839b2641e0a2d4
ram4ik <ram4ik@hotmail.com>
2024-03-15 11:16:35 +0200

    Initial Commit

---
+++ SwiftUITasksBlockingTheMainActor/SwiftUITasksBlockingTheMainActorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITasksBlockingTheMainActorApp.swift
+//  SwiftUITasksBlockingTheMainActor
+//
+//  Created by Ramill Ibragimov on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITasksBlockingTheMainActorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
